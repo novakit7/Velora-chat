45251d8062f3507b61db823056fcd1c13a648c1e
ankit verma <ankivermajnva@gmail.com>
2026-06-26 09:39:11 +0530

    feat(server): initialize server with package.json and basic structure
    - Added package.json for server with dependencies: cors, dotenv, express, mongoose.
    - Created app.js and index.js files in the server/src directory.
    - Removed outdated structure.txt file.

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-(--fg-d)">
+        Velora
+      </h1>
+    </div>
+  );
+}
+
+export default App;
